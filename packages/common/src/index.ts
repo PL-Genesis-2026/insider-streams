3fe099ae49695fbc70f88126bf634b60a61347c9
Pattisier <Principursa@protonmail.com>
2026-03-05 13:44:45 -0500

    package, docs and test updates

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -5,4 +5,4 @@ export { secretMarketplaceAbi, simpleMarketAbi, mockUsdcAbi } from "./generated.
 // NOTE: Update these when new contracts are deployed.
 export const MOCK_USDC_ADDRESS = "0xA75c910D441C99bA651a70451D3bE1d690c1DD85" as const;
 export const SIMPLE_MARKET_ADDRESS = "0xF9EDB9268E01301eE5Ccc985cDb3D825f05a4079" as const;
-export const SECRET_MARKETPLACE_ADDRESS = "0xD0Ad321ab6c124C211Acd0bf98f569b2950F3c9b" as const;
+export const SECRET_MARKETPLACE_ADDRESS = "0xED53f1AE2Ee56ca0fDAA79f0e050E873FCF86616" as const;
