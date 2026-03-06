259a6ec672e730e4d492f373ee5a97fb356c0117
Pattisier <Principursa@protonmail.com>
2026-03-06 14:13:13 -0500

    Create client for Chainlink's private transactions rest api for use in frontend + e2e

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -15,3 +15,11 @@ export const SIMPLE_MARKET_ADDRESS =
   "0x89F02f5a5162570F1a79C5C505b6CA78D13229e7" as const;
 export const SECRET_MARKETPLACE_ADDRESS =
   "0xda55F6bc945CCA8A92e938c05bba58B934892a3c" as const;
+
+// Compliant Private Token addresses (Eth Sepolia)
+export const SIMPLE_TOKEN_ADDRESS =
+  "0xB308Ef20527c5215ec2B2B10F52b311f3AAc6EEB" as const;
+export const POLICY_ENGINE_ADDRESS =
+  "0xb208a00A90839246C9f6008EaDD71177e78D4EA1" as const;
+export const VAULT_ADDRESS =
+  "0xE588a6c73933BFD66Af9b4A07d48bcE59c0D2d13" as const;
